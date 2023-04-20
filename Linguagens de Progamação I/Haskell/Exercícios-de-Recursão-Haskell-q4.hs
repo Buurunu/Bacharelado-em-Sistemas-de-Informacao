3cbc823f7b8d4306f22624f86f34e71967fef4b8
henriquefont <henriquefontoura.exe@gmail.com>
2023-04-20 11:38:50 -0300

    Prolog

--- Linguagens de Progamação I/Haskell/Exercícios-de-Recursão-Haskell-q4.hs
+++ Linguagens de Progamação I/Haskell/Exercícios-de-Recursão-Haskell-q4.hs
@@ -1,13 +1,12 @@
-main :: IO()
+main :: IO ()
 pot :: Float -> Float -> Float
-pot a b 
+pot a b
   | b == 0 = 1
-  | b > 0 = a * pot a (b-1)
-  | b < 0 = 1/pot a (-b)
-
+  | b > 0 = a * pot a (b - 1)
+  | b < 0 = 1 / pot a (-b)
 main = do
   putStrLn ("\nDigite o valor A.")
   a <- readLn
   putStrLn ("\nDigite o valor B.")
-  b <- readLn 
+  b <- readLn
   putStrLn ("\nA elevado a B = " ++ show (pot a b))
